eb9c25d417ec6eaee77f27ed2ee8c3485a6a99e6
Shiyu <felixfeng520@gmail.com>
2025-02-11 02:30:08 +0800

    1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,24 +3,22 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Correct implementation of nextNumber based on Collatz conjecture. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 0) {  // 如果 n 是偶数
+            return n / 2;
+        } else {  // 如果 n 是奇数
             return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5;  // 可以更改初始值来测试其他数字
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
+        System.out.println();  // 换行
     }
 }
